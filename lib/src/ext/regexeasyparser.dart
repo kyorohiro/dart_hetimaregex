36b4b37dc86777c4d7de81dabcefcc239dd06e5a
kyorohiro <kyorohiro@gmail.com>
2015-05-04 00:59:43 +0900

    ..

--- lib/src/ext/regexeasyparser.dart
+++ lib/src/ext/regexeasyparser.dart
@@ -4,7 +4,7 @@ class RegexEasyParser extends heti.EasyParser {
   RegexEasyParser(heti.HetimaBuilder builder) : super(builder) {}
 
   async.Future<List<List<int>>> readFromCommand(List<RegexCommand> command) {
-    RegexVM vm = new RegexVM.createFromCommand([]);
+    RegexVM vm = new RegexVM.createFromCommand(command);
     return vm.lookingAtFromEasyParser(this);
   }
 }
